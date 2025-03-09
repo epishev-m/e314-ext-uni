be84a65f936b0b9e8bb35c45b8ebec2f30a7a3a4
Maksim Epishev <epishev.m@mail.ru>
2025-03-09 14:09:52 +0300

    feat: Add Bootstrap

---
+++ UPM/Runtime/Bootstrap/Bootstrap.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace E314.GameKit.Uni
+{
+
+public abstract class Bootstrap : ScriptableObject
+{
+	internal static Bootstrap Instance { get; private set; }
+
+#if UNITY_EDITOR
+	internal void ExecuteAwake()
+	{
+		Awake();
+	}
+#endif
+
+	internal void Initialize(RuntimeInitializeLoadType type)
+	{
+		switch (type)
+		{
+			case RuntimeInitializeLoadType.SubsystemRegistration:
+				OnSubsystemRegistration();
+				break;
+			case RuntimeInitializeLoadType.AfterSceneLoad:
+				OnAfterAssembliesLoaded();
+				break;
+			case RuntimeInitializeLoadType.BeforeSceneLoad:
+				OnBeforeSplashScreen();
+				break;
+			case RuntimeInitializeLoadType.AfterAssembliesLoaded:
+				OnBeforeSceneLoad();
+				break;
+			case RuntimeInitializeLoadType.BeforeSplashScreen:
+				OnAfterSceneLoad();
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(type), type, null);
+		}
+	}
+
+	protected abstract void OnSubsystemRegistration();
+
+	protected abstract void OnAfterAssembliesLoaded();
+
+	protected abstract void OnBeforeSplashScreen();
+
+	protected abstract void OnBeforeSceneLoad();
+
+	protected abstract void OnAfterSceneLoad();
+
+	private void Awake()
+	{
+#if UNITY_EDITOR
+		if (!Application.isPlaying)
+		{
+			var preloadedAssets = UnityEditor.PlayerSettings.GetPreloadedAssets().ToList();
+			preloadedAssets.RemoveAll(x => x is Bootstrap);
+			preloadedAssets.Add(this);
+			UnityEditor.PlayerSettings.SetPreloadedAssets(preloadedAssets.ToArray());
+			return;
+		}
+#endif
+		Instance = this;
+		Debug.LogError("Awake");
+	}
+}
+
+}
---
+++ UPM/Runtime/Bootstrap/BootstrapRuntime.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace E314.GameKit.Uni
+{
+
+internal static class BootstrapRuntime
+{
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+	private static void RuntimeInitialize_SubsystemRegistration()
+	{
+#if UNITY_EDITOR
+		var preloadAsset = UnityEditor.PlayerSettings.GetPreloadedAssets().FirstOrDefault(x => x is Bootstrap);
+		if (preloadAsset is Bootstrap instance) instance.ExecuteAwake();
+#endif
+
+			Bootstrap.Instance.Initialize(RuntimeInitializeLoadType.SubsystemRegistration);
+	}
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+	private static void RuntimeInitialize_AfterAssembliesLoaded()
+	{
+		Bootstrap.Instance.Initialize(RuntimeInitializeLoadType.AfterAssembliesLoaded);
+	}
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+	private static void RuntimeInitialize_BeforeSplashScreen()
+	{
+		Bootstrap.Instance.Initialize(RuntimeInitializeLoadType.BeforeSplashScreen);
+	}
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	private static void RuntimeInitialize_BeforeSceneLoad()
+	{
+		Bootstrap.Instance.Initialize(RuntimeInitializeLoadType.BeforeSceneLoad);
+	}
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+	private static void RuntimeInitialize_AfterSceneLoad()
+	{
+		Bootstrap.Instance.Initialize(RuntimeInitializeLoadType.AfterSceneLoad);
+	}
+}
+
+}
